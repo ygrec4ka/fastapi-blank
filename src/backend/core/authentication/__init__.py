29b77aa8f54989f3a8385a76c9d9e6df524725f7
ygrec4ka <sam06ser@mail.ru>
2026-03-31 18:03:10 +0400

    feat: fix imports

--- src/backend/core/authentication/__init__.py
+++ src/backend/core/authentication/__init__.py
@@ -1,10 +1,7 @@
 from .transport import bearer_transport
 from .user_manager import UserManager
-from .fastapi_users import fastapi_users, current_user
 
 __all__ = [
     "bearer_transport",
     "UserManager",
-    "fastapi_users",
-    "current_user",
 ]
--- src/backend/core/authentication/fastapi_users.py
+++ src/backend/core/authentication/fastapi_users.py
@@ -1,7 +1,7 @@
 from fastapi_users import FastAPIUsers
 
 from backend.core.models import User
-from backend.core.types.user_id import UserIdType
+from backend.core.types import UserIdType
 
 from backend.api.dependencies.authentication import get_user_manager
 from backend.api.dependencies.authentication import authentication_backend
